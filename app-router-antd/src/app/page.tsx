72e50af7f4f5e7deb168d2509d60b462fd06b5d6
Bailey Miller <baileymiller@live.com>
2024-06-05 11:38:32 -0400

    init commit

---
+++ app-router-antd/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { DownloadOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+
+export default function Home() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <Button type="primary">
+                <DownloadOutlined /> Button
+            </Button>
+        </main>
+    );
+}
